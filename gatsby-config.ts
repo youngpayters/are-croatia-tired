1587e276c400689ef88a1f6ae971c1355487c652
youngpayters <darylpet@gmail.com>
2022-12-09 17:29:25 +0000

    removing default gatsby welcome page

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,25 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
+const siteAddress = new URL("https://www.arecroatiatired.com");
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Are Croatia Tired`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: siteAddress.href,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "are-croatia-tired",
+        protocol: siteAddress.protocol.slice(0, -1),
+        hostname: siteAddress.hostname,
+      },
+    },
+  ],
+};
 
-export default config
+export default config;
