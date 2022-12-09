da49681254157d2da4b2bced9f2474cbaad596b7
youngpayters <darylpet@gmail.com>
2022-12-09 23:16:38 +0000

    now with random text generator

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
-const siteAddress = new URL("https://www.arecroatiatired.com");
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const siteAddress = new URL(process.env.GATSBY_WEBSITE_ADDRESS_URL as string);
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -14,7 +19,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-s3`,
       options: {
-        bucketName: "are-croatia-tired",
+        bucketName: process.env.GATSBY_S3_BUCKET_NAME as string,
         protocol: siteAddress.protocol.slice(0, -1),
         hostname: siteAddress.hostname,
       },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
+import { IGif } from "@giphy/js-types";
+import { Gif } from "@giphy/react-components";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { useState } from "react";
+import { useAsync } from "react-async-hook";
 
 const pageStyles = {
+  display: "flex",
+  flex: "row",
+  justifyContent: "center",
   color: "#232129",
   padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
@@ -11,22 +19,52 @@ const headingStyles = {
   marginBottom: 64,
   maxWidth: 320,
 };
+const containerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+};
+
+const gifIds = [
+  "KCY0yALOOQ5gP6l1a5",
+  "d7BRHyjT329N0ppVsH",
+  "VgHL4NnWOVCg0eDYuV",
+  "5WISrqeKbxS0mXlhT7",
+];
+
+const messages = ["Nope!", "Nikada", "Never", "Not a hope", "Never.", "no"];
+
+function getRndInteger(minimum: number, maximum: number) {
+  return Math.floor(Math.random() * (maximum - minimum)) + minimum;
+}
+
+const giphyFetch = new GiphyFetch(process.env.GATSBY_GIPHY_API_KEY ?? "");
+
+function GifComponent() {
+  const [gif, setGif] = useState<IGif | null>(null);
+  useAsync(async () => {
+    const { data } = await giphyFetch.gif(
+      gifIds[getRndInteger(0, gifIds.length - 1)]
+    );
+    setGif(data);
+  }, []);
+  return gif && <Gif gif={gif} width={400} />;
+}
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        No, never
-        <br />
-      </h1>
-      <img
-        src="https://media3.giphy.com/media/KCY0yALOOQ5gP6l1a5/giphy-downsized.gif?cid=7053c1daqx6nsqa9wzcbjf18ql43b1u2gu3juyo81zsjzigz&rid=giphy-downsized.gif&ct=g"
-        alt="modric"
-      />
+      <div id="container" style={containerStyle}>
+        <h1 style={headingStyles}>
+          {messages[getRndInteger(0, messages.length)]}
+          <br />
+        </h1>
+        <GifComponent />
+      </div>
     </main>
   );
 };
 
 export default IndexPage;
 
-export const Head: HeadFC = () => <title>Home Page</title>;
+export const Head: HeadFC = () => <title>Are Croatia Tired?</title>;
